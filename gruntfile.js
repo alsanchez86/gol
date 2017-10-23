2693a1a3f523fd7d007bdd2d9bcd0a220b8c5d52
alsanchez86 <sanchezpizarro86@gmail.com>
2017-10-23 13:12:06 -0700

    try jasmine

--- gruntfile.js
+++ gruntfile.js
@@ -1,25 +1,40 @@
+// https://github.com/jsoverson/grunt-contrib-jasmine-example
+
 module.exports = function (grunt) {
-    'use strict'
+    'use strict';
 
     // Force use of Unix newlines
     grunt.util.linefeed = '\n';
-
-    grunt.loadNpmTasks('grunt-contrib-jasmine');
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
         jasmine: {
-            customTemplate: {
-                options: {
-                    specs: 'spec/*.js'                    
-                }
+            src: 'js/**/*.js',
+            options: {
+                specs: 'specs/**/*.spec.js',
+                // template: require('grunt-template-jasmine-requirejs'),
+                // templateOptions: {
+                //     requireConfigFile: 'js/config/require-config.js'
+                // }
+            }
+        },
+        jshint: {
+            all: [
+                'gruntfile.js',
+                'specs/**/*.spec.js'
+            ],
+            options: {
+                jshintrc: '.jshintrc'
             }
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
     // register tasks
-    grunt.registerTask("test", ["jasmine"]);
+    grunt.registerTask('test', ['jshint', 'jasmine']);
 
     // default task
     grunt.registerTask("default", ["test"]);
